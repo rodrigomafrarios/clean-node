76c7fe6235c2e5a32cb29540519d7f4f8f5974d7
Rodrigo Mafra <rodrigomafrarios@gmail.com>
2020-11-26 22:10:51 -0300

    feat: ensure SignUpController return an error if no body provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -14,6 +14,23 @@ describe('SignUp Controller', () => {
 
         const httpResponse = controller.handle(httpRequest)
 
-        expect(httpResponse.statusCode).toBe(400)
+		expect(httpResponse.statusCode).toBe(400)
+		expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+	})
+	test('Should return 400 if no email provided', () => {
+        const controller = new SignUpController()
+        const httpRequest = {
+            body: {
+                name: 'any',
+                email: 'anmy@email.com',
+                password: 'anypass',
+                passwordConfirmation: 'any confirmation'
+            }
+        }
+
+        const httpResponse = controller.handle(httpRequest)
+
+		expect(httpResponse.statusCode).toBe(400)
+		expect(httpResponse.body).toEqual(new Error('Missing param: name'))
     })
 })

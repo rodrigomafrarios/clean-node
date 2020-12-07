b26857ea93cd957f8986965cedf737cc8ccd6667
Rodrigo Mafra <rodrigomafrarios@gmail.com>
2020-12-07 17:55:49 -0300

    feat: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -93,4 +93,19 @@ describe('DbAddAccount Usecase', () => {
 		const promise = sut.add(accountData)
 		await expect(promise).rejects.toThrow()
 	})
+	test('Should return an account on success', async () => {
+		const { sut } = factory()
+		const accountData = {
+			name: 'valid_name',
+			email: 'valid_email@mail.com',
+			password: 'valid_password'
+		}
+		const account = await sut.add(accountData)
+		expect(account).toEqual({
+			id: 'valid_id',
+			name: 'valid_name',
+			email: 'valid_email@mail.com',
+			password: 'hashed_password'
+		})
+	})
 })

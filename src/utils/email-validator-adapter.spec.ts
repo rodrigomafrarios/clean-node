8cf9b33d34f1c81c8d590892a87c62f591ccd802
Rodrigo Mafra <rodrigomafrarios@gmail.com>
2020-12-07 14:39:21 -0300

    refactor: move instance of email validator adapter to factory

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,4 +1,4 @@
-import { EmailValidatorAdapter } from './emal-validator'
+import { EmailValidatorAdapter } from './emal-validator-adapter'
 import validator from 'validator'
 
 jest.mock('validator', () => ({
@@ -7,20 +7,24 @@ jest.mock('validator', () => ({
 	}
 }))
 
+const factory = (): EmailValidatorAdapter => {
+	return new EmailValidatorAdapter()
+}
+
 describe('EmailValidator Adapter', () => {
 	test('Should return false if validator returns false', () => {
-		const sut = new EmailValidatorAdapter()
+		const sut = factory()
 		jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
 		const isValid = sut.isValid('invalid_email')
 		expect(isValid).toBe(false)
 	})
 	test('Should return true if validator returns true', () => {
-		const sut = new EmailValidatorAdapter()
+		const sut = factory()
 		const isValid = sut.isValid('valid_email@mail.com')
 		expect(isValid).toBe(true)
 	})
 	test('Should call validator with correct', () => {
-		const sut = new EmailValidatorAdapter()
+		const sut = factory()
 		const isEmailSpy = jest.spyOn(validator, 'isEmail')
 		sut.isValid('valid_email@mail.com')
 		expect(isEmailSpy).toHaveBeenCalledWith('valid_email@mail.com')

bedf33892352601a3befbdc6a7658447724bf691
Rodrigo Mafra <rodrigomafrarios@gmail.com>
2020-12-07 16:20:49 -0300

    refactor: move encrypter new instance to factory

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,13 +1,31 @@
+import { Encrypter } from '../../protocols/encrypter'
 import { DbAddAccount } from './db-add-account'
+
+interface SutTypes {
+	sut: DbAddAccount
+	encrypterStub: Encrypter
+}
+
+const factoryEncrypter = (): Encrypter => {
+	class EncrypterStub {
+		async encrypt (value: string): Promise<string> {
+			return new Promise(resolve => resolve('hashed_password'))
+		}
+	}
+	return new EncrypterStub()
+}
+
+const factory = (): SutTypes => {
+	const encrypterStub = factoryEncrypter()
+	const sut = new DbAddAccount(encrypterStub)
+	return {
+		sut,
+		encrypterStub
+	}
+}
 describe('DbAddAccount Usecase', () => {
 	test('Should call Encrypter with correct password', async () => {
-		class EncrypterStub {
-			async encrypt (value: string): Promise<string> {
-				return new Promise(resolve => resolve('hashed_password'))
-			}
-		}
-		const encrypterStub = new EncrypterStub()
-		const sut = new DbAddAccount(encrypterStub)
+		const { sut, encrypterStub } = factory()
 		const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
 		const accountData = {
 			name: 'valid_name',

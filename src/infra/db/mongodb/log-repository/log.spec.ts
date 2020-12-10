7caccd418c49a659f8d0ba59f32bc5f2e15d0a04
Rodrigo Mafra <rodrigomafrarios@gmail.com>
2020-12-10 11:35:57 -0300

    feat: ensure LogMongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,23 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { LogMongoRepository } from '../log-repository/log'
+describe('Log Mongo Repository', () => {
+	let collection: Collection
+
+	beforeAll(async () => {
+		await MongoHelper.connect('mongodb://0.0.0.0:27017/jest')
+	})
+	afterAll(async () => {
+		await MongoHelper.disconnect()
+	})
+	beforeEach(async () => {
+		collection = await MongoHelper.getCollection('errors')
+		await collection.deleteMany({})
+	})
+	test('Should create an error log on success', async () => {
+		const sut = new LogMongoRepository()
+		await sut.logError('any_error')
+		const count = await collection.countDocuments()
+		expect(count).toBe(1)
+	})
+})

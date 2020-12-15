4e28e810f6c8c7313d845d21ccb4ccd3b90002d1
Rodrigo Mafra <rodrigomafrarios@gmail.com>
2020-12-15 18:25:37 -0300

    feat: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,9 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+let collection: Collection
 
 beforeAll(async () => {
 	await MongoHelper.connect('mongodb://0.0.0.0:27017/jest')
@@ -9,7 +12,7 @@ afterAll(async () => {
 	await MongoHelper.disconnect()
 })
 beforeEach(async () => {
-	const collection = await MongoHelper.getCollection('accounts')
+	collection = await MongoHelper.getCollection('accounts')
 	await collection.deleteMany({})
 })
 
@@ -22,6 +25,23 @@ describe('POST /signup', () => {
 			email: 'rodrigomafrarios@gmail.com',
 			password: '123',
 			passwordConfirmation: '123'
+		})
+		.expect(200)
+	})
+})
+describe('POST /login', () => {
+	test('Should return 200 on login', async () => {
+		const password = await hash('123',12)
+		await collection.insertOne({
+			name: 'Rodrigp',
+			email: 'rodrigomafrarios@gmail.com',
+			password
+		})
+		await request(app)
+		.post('/api/login')
+		.send({
+			email: 'rodrigomafrarios@gmail.com',
+			password: '123'
 		})
 		.expect(200)
 	})

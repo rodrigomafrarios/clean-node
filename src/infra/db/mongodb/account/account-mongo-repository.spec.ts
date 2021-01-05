ef27695c129955e8e0bfe3a42814ecd17d74f889
Rodrigo Mafra <rodrigomafrarios@gmail.com>
2021-01-05 20:10:31 -0300

    feat: ensure Account Mongo Repository return an account on loadByToken without role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -69,4 +69,27 @@ describe('Account Mongo Repository', () => {
 			expect(account.accessToken).toBe('any_token')
 		})
 	})
+	describe('loadByToken()', () => {
+		test('Should return an account on loadByToken without role', async () => {
+			const sut = makeSut()
+			await collection.insertOne({
+				name: 'any_name',
+				email: 'any_mail@mail.com',
+				password: 'any_password',
+				accessToken: 'any_token'
+			})
+			const account = await sut.loadByToken('any_token')
+			expect(account).toBeTruthy()
+			expect(account.id).toBeTruthy()
+			expect(account.name).toBe('any_name')
+			expect(account.email).toBe('any_mail@mail.com')
+			expect(account.password).toBe('any_password')
+			expect(account.accessToken).toBe('any_token')
+		})
+		test('Should return null if loadByEmail fails', async () => {
+			const sut = makeSut()
+			const account = await sut.loadByEmail('any_mail@mail.com')
+			expect(account).toBeFalsy()
+		})
+	})
 })

2224a6daaca6e1e5173263553209da480a60f36d
Rodrigo Mafra <rodrigomafrarios@gmail.com>
2021-01-09 16:28:56 -0300

    refactor: move load-surveys-controller to a folder

---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -0,0 +1,21 @@
+import { Controller, HttpRequest, HttpResponse, LoadSurveys } from './load-surveys-controller-protocols'
+import { ok, serverError, noContent } from '../../../helpers/http/http-helper'
+
+export class LoadSurveysController implements Controller {
+	private readonly loadSurveys: LoadSurveys
+	constructor (loadSurveys: LoadSurveys) {
+		this.loadSurveys = loadSurveys
+	}
+
+	async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+		try {
+			const surveys = await this.loadSurveys.load()
+			if (!surveys.length) {
+				return noContent()
+			}
+			return ok(surveys)
+		} catch (error) {
+			return serverError(error)
+		}
+	}
+}

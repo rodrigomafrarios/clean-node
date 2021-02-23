2f4eecbc5bb9cc702fd1df318a3af24880944298
Rodrigo Mafra <rodrigomafrarios@gmail.com>
2021-02-22 22:08:17 -0300

    feat: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct value

---
+++ src/data/usecases/save-survey-result/db-survey-result.spec.ts
@@ -0,0 +1,51 @@
+import { SaveSurveyResultRepository, SurveyResultModel } from './db-survey-result-protocols'
+import { SaveSurveyResultModel } from '@/domain/usecases/save-survey-result'
+import { DbSaveSurveyResult } from './db-survey-result'
+import MockDate from 'mockdate'
+
+const makeFakeSurveyResultData = (): SaveSurveyResultModel => ({
+	accountId: 'any-account-id',
+	surveyId: 'any-survey-id',
+	answer: 'any-answer',
+	date: new Date()
+})
+
+const makeFakeSurveyResult = (): SurveyResultModel => Object.assign({}, makeFakeSurveyResultData(), { id: 'any-id' })
+
+interface SutTypes {
+	sut: DbSaveSurveyResult
+	saveSurveyResultRepositoryStub: SaveSurveyResultRepository
+}
+
+const makeSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
+	class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
+			async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+					return new Promise(resolve => resolve(makeFakeSurveyResult()))
+			}
+	}
+	return new SaveSurveyResultRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {
+    const saveSurveyResultRepositoryStub = makeSaveSurveyResultRepository()
+    const sut = new DbSaveSurveyResult(saveSurveyResultRepositoryStub)
+    return {
+        sut,
+        saveSurveyResultRepositoryStub
+    }
+}
+
+describe('DbSaveSurveyResult Usecase', () => {
+	beforeAll(() => {
+		MockDate.set(new Date())
+	})
+	afterAll(() => {
+		MockDate.reset()
+	})
+	test('Should call SaveSurveyResultRepository with correct values', async () => {
+		const { sut, saveSurveyResultRepositoryStub } = makeSut()
+		const saveSpy = jest.spyOn(saveSurveyResultRepositoryStub, 'save')
+		await sut.save(makeFakeSurveyResult())
+		expect(saveSpy).toHaveBeenCalledWith(makeFakeSurveyResult())
+	})
+})

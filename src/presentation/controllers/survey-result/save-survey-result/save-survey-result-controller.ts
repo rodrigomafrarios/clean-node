538ef5ec1db5975f9787398c2d58c0a6a8d7a77c
Rodrigo Mafra <rodrigomafrarios@gmail.com>
2021-03-06 17:14:55 -0300

    feat: ensure SaveSurveyResultController returns 500 if LoadSurveyById throws

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,16 +1,19 @@
-import { forbidden, InvalidParamError } from '../../survey/add-survey/add-survey-controller-protocols'
+import { forbidden, InvalidParamError, serverError } from '../../survey/add-survey/add-survey-controller-protocols'
 import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './save-survey-result-protocols.'
 
 export class SaveSurveyResultController implements Controller {
 	constructor (private readonly loadSurveyById: LoadSurveyById) {}
 
 	async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-		const { params } = httpRequest
-		const survey = await this.loadSurveyById.loadById(params.surveyId)
-
-		if (!survey) {
-			return forbidden(new InvalidParamError('surveyId'))
+		try {
+			const { params } = httpRequest
+			const survey = await this.loadSurveyById.loadById(params.surveyId)
+			if (!survey) {
+				return forbidden(new InvalidParamError('surveyId'))
+			}
+			return null
+		} catch (error) {
+			return serverError(error)
 		}
-		return null
 	}
 }
--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result.spec.ts
@@ -1,4 +1,4 @@
-import { forbidden, InvalidParamError } from '../../survey/add-survey/add-survey-controller-protocols'
+import { forbidden, serverError, InvalidParamError } from '../../survey/add-survey/add-survey-controller-protocols'
 import { SaveSurveyResultController } from './save-survey-result-controller'
 import { HttpRequest, LoadSurveyById, SurveyModel } from './save-survey-result-protocols.'
 
@@ -54,5 +54,13 @@ describe('SaveSurveyResult Controller', () => {
 		jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(new Promise(resolve => resolve(null)))
 		const httpResponse = await sut.handle(makeFakeRequest())
 		expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')))
+	})
+
+	test('Should return 500 if LoadSurveyById throws', async () => {
+		const { sut, loadSurveyByIdStub } = makeSut()
+		jest.spyOn(loadSurveyByIdStub, 'loadById')
+		.mockRejectedValueOnce(new Error())
+		const httpResponse = await sut.handle({})
+		expect(httpResponse).toEqual(serverError(new Error()))
 	})
 })

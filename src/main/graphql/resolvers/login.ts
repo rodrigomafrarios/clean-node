408c2af8ecde66f84d25a94676e88afc8b22d693
Rodrigo Mafra <rodrigomafrarios@gmail.com>
2021-03-25 06:32:59 -0300

    feat: add signup mutation

--- src/main/graphql/resolvers/login.ts
+++ src/main/graphql/resolvers/login.ts
@@ -1,8 +1,13 @@
 import { adaptResolver } from '@/main/adapters'
 import { makeLoginController } from '@/main/factories/controllers/login/login-controller-factory'
+import { makeSignUpController } from '@/main/factories/controllers/signup/signup-controller-factory'
 
 export default {
   Query: {
     login: async (parent: any, args: any) => adaptResolver(makeLoginController(), args)
+  },
+
+  Mutation: {
+    signUp: async (parent: any, args: any) => adaptResolver(makeSignUpController(), args)
   }
 }
